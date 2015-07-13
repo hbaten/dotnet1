04e571e34e6a575770f6fa0c583a027ef939cc20
hbaten <baten@gmx.de>
2015-07-13 21:23:24 +0200

    db

--- test.php
+++ test.php
@@ -1,5 +1,18 @@
 <?php
 	
+	function request($req)
+	{
+		global $dbHost,$dbUser,$dbPass,$dbName;
+		$dbHost = "si5wyt7b55.database.windows.net:1433";
+		$dbName = "finestoDBService";$dbUser = "hbaten";$dbPass = "0kulene+";
+		mysql_connect($dbHost,$dbUser,$dbPass);
+		mysql_select_db($dbName);
+		// printLog('SESSION',4,'SQL Query: '.$req);
+		return mysql_query($req);
+	}
+	
 	phpinfo();
+	$result = request("SELECT * FROM _ALL");
+	echo($result);
 	
 	?>
